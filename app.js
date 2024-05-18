6dd3d64f9efd82b1412ab839b13f4fc6d1d3758b
Lucas <lucas.vergara@live.com>
2024-05-18 01:07:23 -0300

    :construction: Working in progress

--- app.js
+++ app.js
@@ -10,7 +10,27 @@ const prevBtn = document.querySelector('.prev-btn');
 //set slides
 container.innerHTML = people.map((person, slideIndex)=>{
     const {img, name, job, text} = person;
+    // more logic
+    let position = 'next';
+    if (slideIndex ===0){
+        position = 'active';
+    }
+    if (slideIndex ===people.lenght - 1) {
+        position = 'last'
+    }
+
     
-    
-    return 'hello'
-});
+    return `<article class="slide ${position}">
+          <img src="${img}" 
+          class="img" 
+          alt="${name}"
+          />
+          <h4>${name}</h4>
+          <p class="title">${job}</p>
+          <p class="title">
+            ${text}
+          </p>
+          <div class="quote-icon">
+            <div class="fas fa-quote-right"></div>
+          </div>`;
+}).join('');
