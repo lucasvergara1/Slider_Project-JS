9b5c8a9e128e00da0013c9a5800bd52b7f17aa97
Lucas <lucas.vergara@live.com>
2024-05-17 23:59:28 -0300

    evolution

---
+++ app.js
@@ -0,0 +1,16 @@
+import people from './data.js';
+
+
+
+const container = document.querySelector('.slide-container');
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn');
+
+
+//set slides
+container.innerHTML = people.map((person, slideIndex)=>{
+    const {img, name, job, text} = person;
+    
+    
+    return 'hello'
+});
